856d449ee1ae4d1fa7e852634326b63b5aa4313d
dima-zharyy <zharyydimka@gmail.com>
2022-08-23 21:54:09 +0300

    added hover on nav links

--- src/components/AppMenu/AuthNav/styles.js
+++ src/components/AppMenu/AuthNav/styles.js
@@ -2,8 +2,14 @@ export const navLinkStyles = {
   color: 'inherit',
   whiteSpace: 'nowrap',
   textDecoration: 'none',
+  transition: 'color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+
   '&.active': {
     color: 'lime',
     borderBottom: '2px solid lime',
+  },
+
+  '&:hover': {
+    color: 'lime',
   },
 };
