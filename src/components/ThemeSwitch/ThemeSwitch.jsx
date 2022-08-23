480f48eeee945430d1792614d741969915150051
dima-zharyy <zharyydimka@gmail.com>
2022-08-23 11:32:30 +0300

    commented unused vars

--- src/components/ThemeSwitch/ThemeSwitch.jsx
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -1,17 +1,17 @@
 import { FormGroup, FormControlLabel, Switch } from '@mui/material';
 import { useState } from 'react';
 
-const light = {
-  palette: {
-    mode: 'light',
-  },
-};
+// const light = {
+//   palette: {
+//     mode: 'light',
+//   },
+// };
 
-const dark = {
-  palette: {
-    mode: 'dark',
-  },
-};
+// const dark = {
+//   palette: {
+//     mode: 'dark',
+//   },
+// };
 
 export const ThemeSwitch = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
